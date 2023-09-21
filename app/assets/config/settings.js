5d75ddba188ac35dddeb46f2bbc1e18e29ea1640
juliagu0 <juliagsquash@gmail.com>
2023-09-20 20:35:42 -0700

    adding error reporting

---
+++ app/assets/config/settings.js
@@ -0,0 +1,14 @@
+const setting = {
+    dev: {
+        apiUrl: 'http://192.168.1.193:9000/api'
+    },
+    staging: {
+        apiUrl: 'http://192.168.1.193:9000/api'
+    },
+
+    production: {
+        apiUrl: 'http://192.168.1.193:9000/api'
+    },
+    
+
+}

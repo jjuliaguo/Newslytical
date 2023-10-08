16c309a4a9f54a6b2c61d43f77727da88bcfa2ad
juliagu0 <juliagsquash@gmail.com>
2023-10-07 19:13:04 -0700

    andriod build

--- app/assets/hooks/useNotifications.js
+++ app/assets/hooks/useNotifications.js
@@ -1,26 +1,25 @@
 import { useEffect } from "react";
-import {Notifications} from "expo-notifications";
+import { Notifications } from "expo";
 import * as Permissions from "expo-permissions";
 import expoPushTokensApi from "../../api/expoPushTokens";
 
+export default useNotifications = (notificationListener) => {
+  useEffect(() => {
+    registerForPushNotification();
+    if (notificationListener)
+      Notifications.addListener(notificationListener);
+  }, []);
 
-export default useNotifications=(notificationListener)=>{
-    useEffect(()=>{
-        registerForPushNotification();
-        if (notificationListener)
-          Notifications.addPushTokenListener(notificationListener)
-        },[]);
+  const registerForPushNotification = async () => {
+    try {
+      const permission = Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (!permission.granted) return;
 
-      const registerForPushNotification = async () => {
-        try {
-          const permission = Permissions.askAsync(Permissions.NOTIFICATIONS);
-          if (!permission.granted) return;
-          
-          const token = await Notifications.getExpoPushTokenAsync();
-          expoPushTokensApi.register(token);
-          console.log(token);
-        } catch (error) {
-          console.log('Error getting a push token', error)
-        }
-      };
-}
+      const token = await Notifications.getExpoPushTokenAsync();
+      expoPushTokensApi.register(token);
+      console.log(token);
+    } catch (error) {
+      console.log("Error getting a push token", error);
+    }
+  };
+};
